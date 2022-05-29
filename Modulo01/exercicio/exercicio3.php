93d05ebc951b8ff24aa9e60d4c64d0d4c0f2e0f2
Elivandro <Elivandro@hotmail.com.br>
2022-05-29 08:53:43 -0400

    Exercicio3 adicionado

---
+++ Modulo01/exercicio/exercicio3.php
@@ -0,0 +1,18 @@
+<?php
+
+    $num = 10;
+
+    if($num > 0){
+
+        echo "Positivo";
+
+    }else if($num < 0){
+
+        echo "Negativo";
+
+    }else{
+
+        echo "Nulo";
+    }
+
+    echo "<br/> pronto, execução Terminou.";
