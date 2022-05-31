481da90972b32852c91e35a808fab7ebbe6b2ff4
Elivandro <Elivandro@hotmail.com.br>
2022-05-31 11:26:11 -0400

    hotfix

--- Modulo01/exercicio/exercicio7.php
+++ Modulo01/exercicio/exercicio7.php
@@ -25,35 +25,35 @@
 <body>
 <?php
     $produto1 = ["id" => 1,
-            "nome" => "<strong>Oneplus 10 Pro</strong>",
+            "nome" => "Oneplus 10 Pro",
             "preco" => "R$ 6.999,00",
             "descricao" => "O <strong>OnePlus 10 Pro</strong> é um smartphone <strong>Android</strong> de bom nível, ótimo para fotos, que pode satisfazer até o mais exigente dos usuários. Tem uma enorme tela <strong>Touchscreen</strong> de <strong>6.7</strong> polegadas e uma resolução de <strong>3216x1440</strong> pixels que é uma das mais altas atualmente em circulação.",
             "imagem" => "./assets/img/Oneplus10-1.jpg"
         ];
 
     $produto2 = ["id" => 2,
-            "nome" => "<strong>Xiaomi Redmi 11T Pro</strong>",
+            "nome" => "Xiaomi Redmi 11T Pro",
             "preco" => "R$ 1.599,99",
             "descricao" => "O <strong>Xiaomi 11T Pro</strong> é um smartphone <strong>Android</strong> com características inovadoras que o tornam uma excelente opção para qualquer tipo de utilização. A tela de <strong>6.67</strong> polegadas coloca esse <strong>Xiaomi</strong> no topo de sua categoria. A resolução também é alta: <strong>2400x1080</strong> pixel.",
             "imagem" => "./assets/img/XiaomiMi-1.jpg"
             ];
 
     $produto3 = ["id" => 3,
-            "nome" => "<strong>Samsung Galaxy S21 Ultra</strong>",
+            "nome" => "Samsung Galaxy S21 Ultra",
             "preco" => "R$ 8.549,10",
             "descricao" => "O <strong>Samsung Galaxy S21</strong> é um smartphone <strong>Android</strong> com características inovadoras que o tornam uma excelente opção para qualquer tipo de utilização. A tela de <strong>6.2</strong> polegadas coloca esse <strong>Samsung</strong> no topo de sua categoria. A resolução também é alta: <strong>2400x1080</strong> pixel.",
             "imagem" => "./assets/img/SamsungS21-1.jpg"
             ];
 
     $produto4 = ["id" => 4,
-            "nome" => "<strong>Apple iPhone 12 Pro Max</strong>",
+            "nome" => "Apple iPhone 12 Pro Max",
             "preco" => "R$ 8.169,05",
             "descricao" => "O <strong>Apple iPhone 12 Pro Max</strong> é um smartphone <strong>iOS</strong> com características inovadoras que o tornam uma excelente opção para qualquer tipo de utilização. A tela de <strong>6.7</strong> polegadas coloca esse <strong>Apple</strong> no topo de sua categoria. A resolução também é alta: <strong>2778x1284</strong> pixel. ",
             "imagem" => "./assets/img/iPhoneProMax-1.jpg"
             ];
 
     $produto5 = ["id" => 5,
-            "nome" => "<strong>Motorola Edge 30</strong>",
+            "nome" => "Motorola Edge 30",
             "preco" => "R$ 5.699,05",
             "descricao" => "O <strong>Motorola Edge 30</strong> é um smartphone <strong>Android</strong> de bom nível, ótimo para fotos, que pode satisfazer até o mais exigente dos usuários. Tem uma enorme tela <strong>Touchscreen</strong> de <strong>6.5</strong> polegadas com uma resolução de <strong>2400x1080</strong> pixel.",
             "imagem" => "./assets/img/MotorolaEdge-1.jpg"
@@ -78,16 +78,16 @@
                     foreach($produtos as $produto){
 
                         echo "<tr>
-                                <td width='240'>{$produto['nome']}</td>
+                                <td width='240'><strong>{$produto['nome']}</strong></td>
                                 <td width='180'>{$produto['preco']}</td>
                                 <td><p>{$produto['descricao']}</p></td>
                                 <td><button type='button' class='btn btn-outline-light' data-bs-toggle='modal' data-bs-target='#JanelaModal{$produto['id']}'><img src='{$produto['imagem']}' width='180'/></button></td>
-                            </tr>";
-                            echo "<div id='JanelaModal{$produto['id']}' class='modal' tabindex='-1' role='dialog'>
+                            </tr>
+                            <div id='JanelaModal{$produto['id']}' class='modal' tabindex='-1' role='dialog'>
                                     <div class='modal-dialog modal-lg' role='document'>
                                         <div class='modal-content'>
                                             <div class='modal-header'>
-                                                <h5 class='modal-title'>{$produto['nome']}</h5>
+                                                <h5 class='modal-title'><strong>{$produto['nome']}</strong></h5>
                                                 <button type='button' class='close btn btn-dark' data-bs-dismiss='modal' aria-label='Close'>
                                                 <span aria-hidden='true'>&times;</span>
                                                 </button>
